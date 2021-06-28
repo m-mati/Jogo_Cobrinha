f0a3bb14ff3f75fdd432a56cd8380bb048b44772
MMati <agendadomarcos@gmail.com>
2021-06-27 21:35:18 -0300

    Game Over

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function criarBG() {
 }
 
 function criarCobrinha(){
-    for(i=0; i< snake.length; i++){
+    for(i=0; i < snake.length; i++){
         context.fillStyle = "yellow";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
@@ -31,18 +31,26 @@ function drawFood(){
 
 document.addEventListener("keydown", update);
 
-function update (event){
-    if(event.keyCode == 37 && direction != "right") direction = "left";
-    if(event.keycode == 38 && direction != "down") direction = "up";
-    if(event.keycode == 39 && direction != "left") direction = "right";
-    if(event.keycode == 40 && direction != "up") direction = "down";
+function update(event){
+    if(event.keyCode == 37 && direction != 'right') direction = 'left';
+    if(event.keyCode == 38 && direction != 'down') direction = 'up';
+    if(event.keyCode == 39 && direction != 'left') direction = 'right';
+    if(event.keyCode == 40 && direction != 'up') direction = 'down';
 }
 
-function iniciarJogo(){
-    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-    if(snake[0].x > 0 && direction == "left") snake[0].x = 16 * box;
-    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-    if(snake[0].y > 0 && direction == "up") snake[0].y = 16 * box;
+function iniciarJogo(){    
+
+    if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+    
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
 
     criarBG();
     criarCobrinha();
@@ -54,10 +62,15 @@ function iniciarJogo(){
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
-    if(direction == "down") snakey += box;
+    if(direction == "down") snakeY += box;
 
-    snake.pop();
-
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop(); //pop tira o último elemento da lista
+    }else{
+        food.x = Math.floor(Math.random() * 15 +1) * box;
+        food.y = Math.floor(Math.random() * 15 +1) * box;
+    }
+    
     let newHead = {
         x: snakeX,
         y: snakeY
