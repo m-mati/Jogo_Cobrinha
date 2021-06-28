8ad7e5d8b47bb727f23a0b8a71440a66342f754f
MMati <agendadomarcos@gmail.com>
2021-06-27 21:42:18 -0300

    estilização do body

--- script.js
+++ script.js
@@ -65,12 +65,12 @@ function iniciarJogo(){
     if(direction == "down") snakeY += box;
 
     if(snakeX != food.x || snakeY != food.y){
-        snake.pop(); //pop tira o último elemento da lista
+        snake.pop();
     }else{
         food.x = Math.floor(Math.random() * 15 +1) * box;
         food.y = Math.floor(Math.random() * 15 +1) * box;
     }
-    
+
     let newHead = {
         x: snakeX,
         y: snakeY
